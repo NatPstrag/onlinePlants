a9e6ae81cf56736501a45fc71db7955442a801a9
NatPstrag <nati.pstrag@gmail.com>
2021-11-19 19:34:36 +0100

    0.7

--- server/server.js
+++ server/server.js
@@ -59,36 +59,63 @@ app.get("/api/v1/products/:id", async (req, res) =>{
 
 //Create a product
 
-app.post("/api/v1/products", (req, res) =>
-{
-    console.log(req.body)
+app.post("/api/v1/products", async (req, res) =>
+{console.log(req.body)
 
-     res.status(201).json({
-        status: "success",
-        data: {
-            product: "maranta"
-        }
-    })
+    try{
+        const results = await db.query
+        ("INSERT INTO products (name, price, units_in_stock, description, images) values ($1, $2, $3, $4, $5) returning *", 
+        [req.body.name, req.body.price, req.body.units_in_stock, req.body.description, req.body.images]);
+console.log(results)
+        res.status(201).json({
+            status: "success",
+            data: {
+                products: results.rows[0],
+            }
+        })
+
+    }catch (err){
+        console.log(err)
+    }
 });
 //Update product
 
-app.put("/api/v1/products/:id" , (req, res) => {
-    console.log(req.params.id);
-    console.log(req.body);
+app.put("/api/v1/products/:id" , async (req, res) => {
+try{
+    const results = await db.query
+    ("UPDATE products SET name = $1, price = $2, units_in_stock=$3, description = $4, images = $5 where id= $6 returning *",
+     [req.body.name, req.body.price, req.body.units_in_stock, req.body.description, req.body.images, req.params.id]);
 
-     res.status(200).json({
+     console.log(results);
+
+        res.status(200).json({
         status: "success",
         data: {
-            product: "maranta"
+            products: results.rows[0],
         }
     });
+
+}catch(err)
+{
+    console.log(err)
+}
+   console.log(req.params.id);
+    console.log(req.body);
 });
 
 //Delete product
 app.delete("/api/v1/products/:id", (req, res) =>{
+try{
     res.status(204).json({
         status: "success"
     });
+
+}catch(err)
+{
+    console.log(err)
+}
+
+    
 });
 
 
